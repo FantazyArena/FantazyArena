f7a708aa7a2f37c0843737951cfc1efb6e086873
MO-AM1R <me.mohamed.amir@gmail.com>
2026-05-11 09:20:19 -0700

    Re-Organized application architecture to MVP deign pattern with clean architecture.

--- fantazy/Presenetation/Leagues/Model/League.swift
+++ fantazy/Presenetation/Leagues/Model/League.swift
@@ -17,14 +17,3 @@ struct League {
         self.countryName = countryName
     }
 }
-
-
-class Sport{
-    let name: String
-    let thumbnail: String
-    
-    init(name: String, thumbnail: String) {
-        self.name = name
-        self.thumbnail = thumbnail
-    }
-}
