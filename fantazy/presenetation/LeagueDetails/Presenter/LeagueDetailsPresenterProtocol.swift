1120f6bca201904aae65030f6dcc4f2674d7c44e
Ahmed El Sayyad <elsayyada197@gmail.com>
2026-05-11 09:51:17 -0700

    Add league details presenter layer

--- fantazy/presenetation/LeagueDetails/Presenter/LeagueDetailsPresenterProtocol.swift
+++ fantazy/presenetation/LeagueDetails/Presenter/LeagueDetailsPresenterProtocol.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+protocol LeagueDetailsViewProtocol: AnyObject {
+    func reloadTeams()
+    func reloadEvents()
+    func updateFavoriteState(isFavorite: Bool)
+}
+
+protocol LeagueDetailsPresenterProtocol: AnyObject {
+    var view: LeagueDetailsViewProtocol? { get set }
+    func viewDidLoad()
+    func toggleFavorite()
+    func numberOfTeams() -> Int
+    func numberOfEvents() -> Int
+    func getTeam(at index: Int) -> Team
+    func getEvent(at index: Int) -> Event
+}
