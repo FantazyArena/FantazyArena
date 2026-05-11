41c948bec4ad825298e9f8057721e6cded871f31
Ahmed El Sayyad <elsayyada197@gmail.com>
2026-05-11 09:51:05 -0700

    Add league details models

--- fantazy/presenetation/LeagueDetails/Model/Event.swift
+++ fantazy/presenetation/LeagueDetails/Model/Event.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct Event {
+    let id: String
+    let homeTeam: Team
+    let awayTeam: Team
+    let date: String
+    let time: String
+    let stadium: String?
+    let league: String?
+    let status: EventStatus
+}
+
+enum EventStatus {
+    case upcoming
+    case live
+    case finished
+}
