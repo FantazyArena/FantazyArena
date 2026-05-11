be5bad78b201dac2f24ad732c9734346a19d300d
MO-AM1R <me.mohamed.amir@gmail.com>
2026-05-11 07:54:44 -0700

    Merge with favorite feature.

--- fantazy/data/repository/SportsRepositoty.swift
+++ fantazy/data/repository/SportsRepositoty.swift
@@ -25,17 +25,20 @@ class SportsRepositoty: SportsRepositotyProtocol{
 
                 League(
                     name: "Premier LeaguePremier LeaguePremier LeaguePremier LeaguePremier LeaguePremier LeaguePremier LeaguePremier LeaguePremier LeaguePremier League",
-                    badgeURL:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLFXtmUsGjZ0tqRZlxNE5jFWnI72nA108RBA&s"
+                    badgeURL:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLFXtmUsGjZ0tqRZlxNE5jFWnI72nA108RBA&s",
+                    countryName: "England"
                 ),
 
                 League(
                     name: "Champions League",
-                    badgeURL:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVqOch7HSvCKvpY7CeQaIjgPGa1cfT_gRhdw&s"
+                    badgeURL:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVqOch7HSvCKvpY7CeQaIjgPGa1cfT_gRhdw&s",
+                    countryName: "England"
                 ),
 
                 League(
                     name: "League Cup",
-                    badgeURL:"https://www.backsportspage.com/wp-content/uploads/2022/08/2707566-55973350-2560-1440.jpg"
+                    badgeURL:"https://www.backsportspage.com/wp-content/uploads/2022/08/2707566-55973350-2560-1440.jpg",
+                    countryName: "England"
                 )
             ]
 
