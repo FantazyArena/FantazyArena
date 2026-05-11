8371c0950fb7fc9ded8721f46daef3a54f25554b
Ahmed El Sayyad <elsayyada197@gmail.com>
2026-05-11 09:52:26 -0700

    Add teams table view cells

--- fantazy/presenetation/LeagueDetails/Cells/TableViewCells/TeamCell/TeamTableViewCell.swift
+++ fantazy/presenetation/LeagueDetails/Cells/TableViewCells/TeamCell/TeamTableViewCell.swift
@@ -8,16 +8,83 @@
 import UIKit
 
 class TeamTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    var teams: [Team] = []
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setupCollectionView()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    private func setupCollectionView() {
 
-        // Configure the view for the selected state
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.collectionViewLayout = createLayout()
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+
+        collectionView.register(
+            UINib(nibName: "TeamCollectionViewCell", bundle: nil),
+            forCellWithReuseIdentifier: "TeamCollectionViewCell"
+        )
     }
+
     
+    func configure(with teams: [Team]) {
+        self.teams = teams
+        collectionView.reloadData()
+    }
+
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+
+        return UICollectionViewCompositionalLayout { _, _ in
+
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalHeight(1)
+                )
+            )
+
+            let group = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .absolute(140),
+                    heightDimension: .absolute(170)
+                ),
+                subitems: [item]
+            )
+
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            section.interGroupSpacing = 12
+            section.contentInsets = NSDirectionalEdgeInsets(
+                top: 10, leading: 16, bottom: 10, trailing: 16
+            )
+
+            return section
+        }
+    }
+}
+
+extension TeamsTableViewCell: UICollectionViewDataSource, UICollectionViewDelegate {
+
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return teams.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "TeamCollectionViewCell",
+            for: indexPath
+        ) as! TeamCollectionViewCell
+
+        cell.configure(with: teams[indexPath.row])
+        return cell
+    }
 }
