434cae29fb9a179b5fb5bec4ae0a83af93bbc69e
Ahmed El Sayyad <elsayyada197@gmail.com>
2026-05-11 18:16:34 -0700

    feat: add FavoritesManager to handle save, remove, and fetch favorite league with No Implementation yet

--- fantazy/Data/Database/FavoritesManager.swift
+++ fantazy/Data/Database/FavoritesManager.swift
@@ -5,4 +5,36 @@
 //  Created by Ahmed El Sayyad Mohamed on 11/05/2026.
 //
 
-import Foundation
+import CoreData
+
+class FavoritesManager {
+    static let shared = FavoritesManager()
+    
+    private let context = PersistenceController.shared.context
+    
+    private init() {}
+    
+    func fetchFavorites() -> [FavoriteLeague] {
+        []
+    }
+    
+    func addFavorite(id: Int, name: String, logo: String, country: String) {}
+    
+    func removeFavorite(id: Int) {}
+    
+    func toggleFavorite(id: Int, name: String, logo: String, country: String) {}
+    
+    func isFavorite(id: Int) -> Bool {
+        return false
+    }
+    
+    private func saveContext() {
+        guard context.hasChanges else { return }
+            do {
+                try context.save()
+            } catch {
+                print("Failed to save context: \(error)")
+            }
+    }
+    
+}
