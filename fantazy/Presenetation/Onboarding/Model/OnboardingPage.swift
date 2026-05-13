21010c444a8c4233c2e8b054e1264f7e47f1f490
Ahmed El Sayyad <elsayyada197@gmail.com>
2026-05-13 13:21:36 -0700

    impelementOnBoarding Screens and update navigation

---
+++ fantazy/Presenetation/Onboarding/Model/OnboardingPage.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingPage.swift
+//  fantazy
+//
+//  Created by Ahmed El Sayyad Mohamed on 13/05/2026.
+//
+
+import Foundation
+
+struct OnboardingPage {
+    let animationName: String
+    let title: String
+    let subtitle: String
+}
--- fantazy/Utils/Navigation/AppCoordinator.swift
+++ fantazy/Utils/Navigation/AppCoordinator.swift
@@ -1,35 +1,46 @@
-//
-//  AppCoordinator.swift
-//  fantazy
-//
-//  Created by Ahmed El Sayyad Mohamed on 11/05/2026.
-//
-
 import UIKit
 
 final class AppCoordinator {
-
     static let shared = AppCoordinator()
-
     private init() {}
 
-
     private var window: UIWindow?
-
     private let tabBarController = UITabBarController()
-
     private let homeNavigationController = UINavigationController()
     private let favoritesNavigationController = UINavigationController()
     private let settingsNavigationController = UINavigationController()
-
 
     func configure(window: UIWindow) {
         self.window = window
     }
 
-
     func start() {
+        if UserDefaults.standard.bool(forKey: "hasSeenOnboarding") {
+            showMainApp(animated: false)
+        } else {
+            showOnboarding()
+        }
+        window?.makeKeyAndVisible()
+    }
 
+    func showOnboarding() {
+        let vc = OnboardingViewController(nibName: "OnboardingViewController", bundle: nil)
+        let presenter = OnboardingPresenter(view: vc)
+        
+        presenter.onFinish = { [weak self] in
+            self?.handleOnboardingFinish()
+        }
+        
+        vc.presenter = presenter
+        window?.rootViewController = vc
+    }
+
+    private func handleOnboardingFinish() {
+        UserDefaults.standard.set(true, forKey: "hasSeenOnboarding")
+        showMainApp(animated: true)
+    }
+
+    func showMainApp(animated: Bool) {
         setupHomeTab()
         setupFavoritesTab()
         setupSettingsTab()
@@ -40,122 +51,51 @@ final class AppCoordinator {
             settingsNavigationController
         ]
 
-        tabBarController.selectedIndex = 0
-
         window?.rootViewController = tabBarController
-        window?.makeKeyAndVisible()
+        
+        if animated, let window = window {
+            UIView.transition(with: window, duration: 0.5, options: .transitionCrossDissolve, animations: nil)
+        }
     }
-}
 
-
-private extension AppCoordinator {
-
-    func setupHomeTab() {
-
-        let vc = HomeViewController(
-            nibName: "HomeViewController",
-            bundle: nil
-        )
-
+    private func setupHomeTab() {
+        let vc = HomeViewController(nibName: "HomeViewController", bundle: nil)
         vc.coordinator = self
-        vc.presenter = HomePresenter(view: vc)
-
         homeNavigationController.viewControllers = [vc]
-
-        homeNavigationController.tabBarItem = UITabBarItem(
-            title: "Home",
-            image: UIImage(systemName: "house"),
-            selectedImage: UIImage(systemName: "house.fill")
-        )
+        homeNavigationController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
     }
 
-    func setupFavoritesTab() {
-
-        let vc = FavoritesViewController(
-            nibName: "FavoritesViewController",
-            bundle: nil
-        )
-
+    private func setupFavoritesTab() {
+        let vc = FavoritesViewController(nibName: "FavoritesViewController", bundle: nil)
         favoritesNavigationController.viewControllers = [vc]
-
-        favoritesNavigationController.tabBarItem = UITabBarItem(
-            title: "Favorites",
-            image: UIImage(systemName: "heart"),
-            selectedImage: UIImage(systemName: "heart.fill")
-        )
+        favoritesNavigationController.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(systemName: "heart"), selectedImage: UIImage(systemName: "heart.fill"))
     }
 
-    func setupSettingsTab() {
-
-        let vc = SettingsViewController(
-            nibName: "SettingsViewController",
-            bundle: nil
-        )
-
+    private func setupSettingsTab() {
+        let vc = SettingsViewController(nibName: "SettingsViewController", bundle: nil)
         settingsNavigationController.viewControllers = [vc]
-
-        settingsNavigationController.tabBarItem = UITabBarItem(
-            title: "Settings",
-            image: UIImage(systemName: "gearshape"),
-            selectedImage: UIImage(systemName: "gearshape.fill")
-        )
+        settingsNavigationController.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gearshape"), selectedImage: UIImage(systemName: "gearshape.fill"))
     }
 }
-
 
 extension AppCoordinator {
-
     func navigateToLeagues(sport: Sport) {
-
-        let vc = LeaguesViewController(
-            nibName: "LeaguesViewController",
-            bundle: nil
-        )
-
-        let repository = SportsRepository.shared
-
-        vc.presenter = LeaguesPresenter(
-            view: vc,
-            repository: repository,
-            sport: sport
-        )
-
+        let vc = LeaguesViewController(nibName: "LeaguesViewController", bundle: nil)
+        vc.presenter = LeaguesPresenter(view: vc, repository: SportsRepository.shared, sport: sport)
         vc.coordinator = self
-
-        homeNavigationController.pushViewController(
-            vc,
-            animated: true
-        )
+        homeNavigationController.pushViewController(vc, animated: true)
     }
 
     func navigateToLeagueDetails(leagueId: String) {
-
-        let vc = LeagueDetailsViewController(
-            nibName: "LeagueDetailsViewController",
-            bundle: nil
-        )
-
+        let vc = LeagueDetailsViewController(nibName: "LeagueDetailsViewController", bundle: nil)
         vc.leagueId = leagueId
         vc.coordinator = self
-
-        homeNavigationController.pushViewController(
-            vc,
-            animated: true
-        )
+        homeNavigationController.pushViewController(vc, animated: true)
     }
 
     func navigateToTeamDetails(team: Team) {
-
-        let vc = TeamDetailsViewController(
-            nibName: "TeamDetailsViewController",
-            bundle: nil
-        )
-
+        let vc = TeamDetailsViewController(nibName: "TeamDetailsViewController", bundle: nil)
         vc.team = team
-
-        homeNavigationController.pushViewController(
-            vc,
-            animated: true
-        )
+        homeNavigationController.pushViewController(vc, animated: true)
     }
 }
