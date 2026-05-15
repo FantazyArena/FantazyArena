ff48d488bb2c82371b8c0ef8cd1fe66cf80c81ec
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-05-15 13:09:34 -0700

    Disable landscape orientation .

--- fantazy/AppDelegate.swift
+++ fantazy/AppDelegate.swift
@@ -11,7 +11,10 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    func application(_ application: UIApplication,
+        supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
